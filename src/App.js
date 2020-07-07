915cb6e322ac80acfb3c6b11e69ca2581b68fa84
okayhan89 <okayhan89@gmail.com>
2020-07-08 00:25:01 +0900

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { debounce } from "lodash";
 import ReactDOM from "react-dom";
 import 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import jQuery  from 'jquery';
+
+//import './js/scripts.js';
+
 
 const element = document.getElementById("main");
 const savedText = localStorage.getItem("saved");
@@ -19,6 +24,15 @@ const defaultValue = savedText || exampleText;
 
 class Example extends React.Component {
 
+  componentDidMount() {
+    // Jquery here $(...)...
+    window.$ = window.jQuery = jQuery;
+    /*!
+    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+  }
   state = {
     readOnly: false,
     dark: localStorage.getItem("dark") === "enabled",
@@ -55,10 +69,29 @@ class Example extends React.Component {
 
     return (
       <div className="container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Clarence Taylor</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                </ul>
+            </div>
+        </nav>        
+        
         <div class="row">
 
         <div className="col-md-2">hihi</div>
         <div className="col-md-10">
+          <form>
           <div>
             <button type="button" onClick={this.handleEditable}>
               {this.state.readOnly ? "Editable" : "Read only"}
@@ -93,6 +126,7 @@ class Example extends React.Component {
               autoFocus
             />
           </div>
+          </form>
         </div>
 
         </div>
